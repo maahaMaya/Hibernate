1d6a87ac2c7d919d65371da52e0f691cba882609
sourav <devbhsourav@gmail.com>
2024-08-23 23:26:16 +0530

    [sourav]add : InsertOperation

---
+++ src/main/java/com/hibernate/utility/hibernate_utility_testing/StudentInsert.java
@@ -0,0 +1,27 @@
+package com.hibernate.utility.hibernate_utility_testing;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.hibernate.entity.Student;
+import com.hibernate.utility.Hibernate6Utility;
+
+
+public class StudentInsert {
+
+	public static void main(String[] args){
+		SessionFactory sessionFactory = Hibernate6Utility.getSessionFactory();
+		Session session = sessionFactory.openSession();
+
+		Transaction transaction =  session.beginTransaction();
+		Student student = new Student();
+		student.setSno(1);
+		student.setSname("HariOm");
+		student.setSmarks(100.00);
+		
+		session.persist(student);
+		transaction.commit();
+		session.close();
+	}
+}
